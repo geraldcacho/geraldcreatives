28f109ddc9d0f5c471edd0dffdc98670d81ed77e
Gerald Cacho <gc.geraldcacho@hotmail.com>
2024-09-03 13:35:30 +0800

    fixed logo issue

--- src/components/header/logo/logo.tsx
+++ src/components/header/logo/logo.tsx
@@ -1,18 +1,25 @@
+import React from 'react';
 import styles from './logo.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Logo() {
-  return(
-    <Link href="/" aria-label="Gerald Cacho" title="Gerald Cacho" className={styles.logo}>
+interface LogoProps {
+  className?: string; // Accept a className prop
+}
+
+const Logo: React.FC<LogoProps> = ({ className }) => {
+  return (
+    <Link href="/" aria-label="Gerald Cacho" title="Gerald Cacho" className={`${styles.logo} ${className || ''}`}>
       <div className={styles.flipper}>
         <div className={`${styles.circle} ${styles.front}`}>
-        <Image className={styles.symbol} src="/img/GC-LOGO-dark.png" width="36" height="20" aria-hidden="true" alt="" />
+          <Image className={styles.symbol} src="/img/GC-LOGO-dark.png" width="36" height="20" aria-hidden="true" alt="" />
         </div>
         <div className={`${styles.circle} ${styles.back}`}>
-          <Image className={styles.avatar} src="/img/gerald-cacho-avatar-sm.png"  aria-hidden="true" width="34" height="60" alt="" />
+          <Image className={styles.avatar} src="/img/gerald-cacho-avatar-sm.png" aria-hidden="true" width="34" height="60" alt="" />
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
+
+export default Logo;
